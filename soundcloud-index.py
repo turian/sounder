60e9a164030522d67ffbf91a7a5cd33abca93dc7
Joseph Turian <turian@gmail.com>
2014-05-27 22:50:30 -0700

    Indexer methods to cache soundcloud information

--- soundcloud-index.py
+++ soundcloud-index.py
@@ -103,26 +103,22 @@ def get_artist_info(soundcloudclient, artist):
     # We have to convert the artist name to an id, so we must run the soundcloudclient
     info = soundcloudclient.get('/users/%s' % artist).obj
     id = info["id"]
+    firebase.put_async("/artists/%s" % id, "info", info)
 
-    if firebase.put_async("/artists/%s" % id, "info", info):
-        return
+# Get a soundcloud artist dict, like "tracks" or "followings", and store it in firebase
+def get_artist_dict(soundcloudclient, q, artist):
+    print ('/artists/%s' % artist)
+    dicts = firebase.get("/artists", artist)
+    if q in dicts:
+        print "(Firebase cached) Found %d %s by artist %s" % (len(dicts[q]), q, artist)
+        return dicts[q]
 
-def get_artist_tracks(soundcloudclient, artist):
-    tracks = firebase.get("/tracks", artist)
-    if tracks:
-        print "Found %d tracks by artist %s" % (len(tracks.values()), artist)
-        return [Track(**v) for v in tracks.values()]
-
-    tracks = _get_soundcloud_list(soundcloudclient, '/users/%s/tracks' % artist)
+    dict = _get_soundcloud_dict(soundcloudclient, '/users/%s/%s' % (artist, q))
 
     # @todo: Don't delete everything. Just insert what's new.
-    firebase.delete("/tracks", artist)
-    for t in tracks:
-        firebase.post_async("/tracks/%s" % artist, t);
-
-    # @todo: Some way to make sure they were all set?
-    
-    return tracks
+    firebase.delete("/artists/%s" % artist, q)
+    firebase.put_async("/artists/%s" % artist, q, dict)
+    return dict
 
 def clips_from_track(t):
     tmpdir = tempfile.mkdtemp()
@@ -205,11 +201,13 @@ if __name__ == "__main__":
     pyechonest.config.ECHO_NEST_API_KEY = CONFIG["ECHO_NEST_API_KEY"]
     soundcloudclient = soundcloud.Client(client_id=CONFIG["SOUNDCLOUD_CLIENT_ID"])
 
-    for artist in CONFIG["SOUNDCLOUD_ARTISTS_TO_INDEX"]:
-        tracks = get_artist_info(soundcloudclient, artist)
+#    for artist in CONFIG["SOUNDCLOUD_ARTISTS_TO_INDEX"]:
+#        tracks = get_artist_info(soundcloudclient, artist)
 
-    for artist_ids in firebase.get("/", "artists"):
-        print artist
+    for artist_id in firebase.get("/", "artists"):
+#        get_artist_dict(soundcloudclient, "followings", artist_id)
+        get_artist_dict(soundcloudclient, "favorites", artist_id)
+        get_artist_dict(soundcloudclient, "tracks", artist_id)
 
 #    tracks = _get_soundcloud_list(soundcloudclient, '/users/%s/tracks' % id)
 #    print tracks
